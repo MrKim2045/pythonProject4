b82f66efd929d6af1f333484657688ea9f84024f
MR.KIM <alfaknyaz@gmail.com>
2021-08-23 22:56:16 +0300

    Initial commit

---
+++ T2.py
@@ -0,0 +1,14 @@
+balance = int(input("Ваш баланс в начале года ") )
+iphone_price = 120000
+coffee_price = 50
+x =12
+zp = int(input("Ежемесячный остаток от зарплаты "))
+balance = balance+zp*x
+
+print(balance,"В конце года")
+if balance > iphone_price:
+  print("УРА! Я могу позволить себе Айфон")
+else:
+  print("Я не смогу позволить себе Айфон")
+  print("Вам не хватает",iphone_price - balance)
+print("Ваш баланс к концу года", balance)

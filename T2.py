2e72e4bae61b41dbeb42b99b7f97af0686b4c8ab
MR.KIM <alfaknyaz@gmail.com>
2021-08-23 23:33:34 +0300

    Небольшие изменения

--- T2.py
+++ T2.py
@@ -1,3 +1,4 @@
+import math
 balance = int(input("Ваш баланс в начале года ") )
 iphone_price = 120000
 coffee_price = 50
@@ -11,5 +12,9 @@ if balance > iphone_price:
   print("УРА! Я могу позволить себе Айфон. ")
 else:
   print("Я не смогу позволить себе Айфон. =(((")
+  nd=iphone_price - balance
   print("Вам не хватает",iphone_price - balance)
+  nw=nd/zp
+  math.ceil (nw, )
+  print((nw) , "Месяцев нужно работать")
 print("Ваш баланс к концу года", balance)

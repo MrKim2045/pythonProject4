63e7d04878e87c3e3454f845a9467273a1b7c2e6
MR.KIM <alfaknyaz@gmail.com>
2021-09-05 14:45:15 +0300

    Все вместе Дано натуральное число. Напишите программу, которая вычисляет:
    сумму его цифр;
    количество цифр в нем;
    произведение его цифр;
    среднее арифметическое его цифр;
    его первую цифру;
    сумму его первой и последней цифры.

--- T4.py
+++ T4.py
@@ -1,15 +1,22 @@
-total = 0
-max1 = 0
-min1 = 9
-n =  int(input())
-while n > 0:
-    total = n % 10
-    if total > max1:
-        max1 = total
-    if total < min1:
-        min1 = total
-    n = n//10
-print('Максимальная цифра равна', max1)
-print('Минимальная цифра равна', min1)
+n = int(input())
+sum = 0
+colv = 0
+proz = 1
+n2 = n
+last_d = n %10
+while n != 0 :
+    last_d1 = n % 10
+    sum+=last_d1
+    colv+=1
+    proz*=last_d1
+    n=n//10
+sa =sum/colv
 
+n1 = (n2//(10**(colv-1)))
 
+print(sum)
+print(colv)
+print(proz)
+print(sa)
+print(n1)
+print(n1+last_d)

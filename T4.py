781f5a6b97219e5ff7ee31a79f70e34d7c3d6ea1
MR.KIM <alfaknyaz@gmail.com>
2021-09-05 13:18:02 +0300

    Новая задача

--- T4.py
+++ T4.py
@@ -1,7 +1,17 @@
-a, k = int(input()), 0
-while a<=5 and a>0:
-    if a == 5:
-        k += 1
-    a = int(input())
-print(k)
+a=int(input())
 
+total=0
+while a!=0:
+    if a>=25:
+        total=total+1
+        a-=25
+    elif a>=10:
+        total+=1
+        a-=10
+    elif a>=5:
+        total+=1
+        a-=5
+    elif a>=1:
+        total+=1
+        a-=1
+print(total)

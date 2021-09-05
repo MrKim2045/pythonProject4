4ff50ff7b7cfd229db5a8185716b96c4576b0018
MR.KIM <alfaknyaz@gmail.com>
2021-09-05 13:53:48 +0300

    next

--- T4.py
+++ T4.py
@@ -1,17 +1,15 @@
-a=int(input())
+total = 0
+max1 = 0
+min1 = 9
+n =  int(input())
+while n > 0:
+    total = n % 10
+    if total > max1:
+        max1 = total
+    if total < min1:
+        min1 = total
+    n = n//10
+print('Максимальная цифра равна', max1)
+print('Минимальная цифра равна', min1)
 
-total=0
-while a!=0:
-    if a>=25:
-        total=total+1
-        a-=25
-    elif a>=10:
-        total+=1
-        a-=10
-    elif a>=5:
-        total+=1
-        a-=5
-    elif a>=1:
-        total+=1
-        a-=1
-print(total)
+
